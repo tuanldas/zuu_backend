dce7160131ecc38dde061332394a9f3e78a0ab82
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-03 13:09:15 +0700

    Add language

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -15,6 +15,6 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Password attempts exceeded. Account frozen for :minutes minutes..',
 
 ];
