5378939b85870b50199d4b5a2e1f555aa9b0492e
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-03 13:09:36 +0700

    Add language vi

---
+++ lang/vi/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Sai email hoặc mật khẩu',
+    'password' => 'Mật khẩu không hợp lệ',
+    'throttle' => 'Bạn đăng nhập sai quá nhiều. Vui lòng thử lại sau :minutes phút ',
+];
