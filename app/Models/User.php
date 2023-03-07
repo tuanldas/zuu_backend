9b3ee3962445b016677042c72e7ed5d1545e6f3d
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-07 17:10:23 +0700

    Add custom column uuid in model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\User\LoginTypeEnums;
 use DateTimeInterface;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,23 +14,24 @@ use Laravel\Sanctum\NewAccessToken;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+    use HasApiTokens, HasFactory, Notifiable, HasUuids;
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
-
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function uniqueIds(): array
+    {
+        return ['uuid'];
+    }
 
     public function createTokenWithTypePassword(string $name, array $abilities = ['*'], DateTimeInterface $expiresAt = null): NewAccessToken
     {
