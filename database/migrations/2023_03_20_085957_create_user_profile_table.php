d5f5f002df405333425af22f02dfd61194f14e1c
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-20 16:05:20 +0700

    Create migrate create table use profile

---
+++ database/migrations/2023_03_20_085957_create_user_profile_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('first_name', 20);
+            $table->string('last_name', 20);
+            $table->text('about');
+            $table->string('designation', 30);
+            $table->string('website', 100);
+            $table->string('phone', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile');
+    }
+};
