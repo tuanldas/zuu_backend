39d688942d7342ae5a208951f000b02d2d49d4de
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-24 12:46:13 +0700

    Update cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,11 +15,11 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $request->headers->set('Accept', 'application/json');
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin' , '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Application');
-        return $response;
+        return $next($request)
+            ->header('Accept', 'application/json')
+            ->header('Access-Control-Allow-Origin', ['*'])
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Application, Origin, X-Auth-Token');
     }
 }
