8d0c21ca857f246d48cbc15889f5482f0ab08678
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-28 04:53:46 +0700

    Create mock project use case

--- tests/Helpers/ProjectHelpers.php
+++ tests/Helpers/ProjectHelpers.php
@@ -3,13 +3,21 @@
 namespace Tests\Helpers;
 
 use App\Models\Project;
+use App\UseCase\Projects\ProjectUseCase;
 
 trait ProjectHelpers
 {
+    public function mockProjectUsecase()
+    {
+        return $this->getMockBuilder(ProjectUseCase::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
     public function createProject($userId): Project
     {
         return Project::factory()->create([
-           'owner' => $userId
+            'owner' => $userId
         ]);
     }
 }
