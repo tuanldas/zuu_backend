2eabb81aef8409e3d2d28b0455e7f9cf72f7c597
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-27 13:11:07 +0700

    Change method encrypt to encryptString

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,8 +23,8 @@ class AuthController extends Controller
             $user = Auth::user();
             $token = $user->createTokenWithTypePassword($user->uuid, Carbon::now()->addSecond(config('custom.token_login_expires_at')));
             return response()->json([
-                'uuid' => Crypt::encrypt($user->uuid),
-                "access_token" => Crypt::encrypt($token->plainTextToken),
+                'uuid' => Crypt::encryptString($user->uuid),
+                "access_token" => Crypt::encryptString($token->plainTextToken),
                 "expires_in" => (int)config('custom.token_login_expires_at'),
             ]);
         }
