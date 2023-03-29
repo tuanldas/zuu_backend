65d39e62015adda9c9646828282c76eda0ff7a62
Lê Đình Tuấn <tuanldas@gmail.com>
2023-03-29 14:45:39 +0700

    Test get user not found

--- tests/Unit/Users/Services/GetUserByUUidTest.php
+++ tests/Unit/Users/Services/GetUserByUUidTest.php
@@ -9,4 +9,22 @@ class GetUserByUUidTest extends TestCase
 {
     use UserHelpers;
 
+    function testGetUserByUUidSuccess()
+    {
+        $user = $this->createUser();
+        $userService = $this->newUserService();
+        $response = $userService->getUserByUserUUid($user->uuid);
+        $this->assertEquals($user->uuid, $response->uuid);
+        $this->assertEquals($user->id, $response->id);
+        $this->assertEquals($user->email, $response->email);
+    }
+
+    function testUserCannotGetUserWhenUserNotExists()
+    {
+
+        $this->createUser();
+        $userService = $this->newUserService();
+        $response = $userService->getUserByUserUUid(fake()->uuid);
+        $this->assertNull($response);
+    }
 }
