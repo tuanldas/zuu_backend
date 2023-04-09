77bb7343d7610976b865ed1260b4e27b02d7fed7
Lê Đình Tuấn <tuanldas@gmail.com>
2023-04-09 22:02:02 +0700

    Import class crypt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Crypt;
 
 class AuthController extends Controller
 {
